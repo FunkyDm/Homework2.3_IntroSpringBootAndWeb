9784df21ace2bffcbb2bf4b8652723c8d6b0b1e3
PoshiloySmih <mrforxbox@mail.ru>
2024-06-21 15:44:57 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringcalculator/SkyproSpringCalculatorApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.skyprospringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCalculatorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SkyproSpringCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringcalculator/SkyproSpringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
